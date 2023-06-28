a21029cdf350cf83fc67fc9264b43461251edc1e
ellehailu <hailu.elle@gmail.com>
2023-06-28 15:52:54 -0700

    update css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ParkService from './js/advice';
+import ParkService from './js/ParkService';
 
 function getParks(activity){
     ParkService.getParks(activity)
@@ -21,6 +21,7 @@ function printElements(response){
         oldList.remove();
     } 
     const newList = document.createElement("ul");
+    newList.setAttribute("class", "container");
     for(let i = 0; i <10; i ++) {
         const li = document.createElement('li');
         li.innerText = `${response.data[0].parks[i].fullName}`;
