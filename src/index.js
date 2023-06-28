d1abc7db3a100634fd44dd1c028dcc3cd2d03e28
ellehailu <hailu.elle@gmail.com>
2023-06-28 15:07:46 -0700

    Fix bug with printElements function

--- src/index.js
+++ src/index.js
@@ -4,36 +4,41 @@ import './css/styles.css';
 import ParkService from './js/advice';
 
 function getParks(activity){
-   ParkService.getParks(activity)
-   .then(function(response){
-    if (response.data){
-      printElements(response);
-    }
-    else{
-      printError(response);
-    }
-   });
+    ParkService.getParks(activity)
+        .then(function(response){
+            if (response.data){
+                printElements(response);
+            }
+            else{
+                printError(response);
+            }
+        });
 }
 
 function printElements(response){
-  for(let i = 0; i <10; i ++) {
-    const li = document.createElement('li');
-   li.innerText = `${response.data[0].parks[i].fullName}`;
-    document.querySelector('#parks').append(li);
-  }
-  // console.log(response.data[0].parks[0].fullName);
-  // document.querySelector('#parks').innerText = `${response.data[0].parks[0].fullName}`;
+    const oldList = document.querySelector("ul");
+    if (oldList !== null) {
+        oldList.remove();
+    } 
+    const newList = document.createElement("ul");
+    for(let i = 0; i <10; i ++) {
+        const li = document.createElement('li');
+        li.innerText = `${response.data[0].parks[i].fullName}`;
+        newList.append(li);
+    }
+    document.querySelector("#results").append(newList);
 }
 
 function printError(error){
-  document.querySelector('#parks').innerText = `${error}`;
+    document.querySelector('#parks').innerText = `${error}`;
 }
 
 
 function handleForm(event) {
-  event.preventDefault();
-  const activity = document.querySelector("#activity").value;
-  getParks(activity);
+    event.preventDefault();
+    const activity = document.querySelector("#activity").value;
+    getParks(activity);
+    document.getElementById('nationalParks').reset();
 }
 
 
--- src/js/advice.js
+++ src/js/advice.js
@@ -1,13 +1,13 @@
 export default class ParkService {
     static getParks(activity) {
-        return fetch(`https://developer.nps.gov/api/v1/activities/parks?q=${activity}&api_key=U71bCKIawRW5iYdIYr7MFKROOLnVAlrralTSnMlm`)
-        .then(function(response) {
-            if (!response.ok) {
-                const errorMessage = `${response.status} ${response.statusText}`;
-                throw new Error(errorMessage);
-            } else {
-                return response.json();
-            }
+        return fetch(`https://developer.nps.gov/api/v1/activities/parks?q=${activity}&api_key=${process.env.API_KEY}`)
+            .then(function(response) {
+                if (!response.ok) {
+                    const errorMessage = `${response.status} ${response.statusText}`;
+                    throw new Error(errorMessage);
+                } else {
+                    return response.json();
+                }
             })
             .catch(function(error) {
                 return error;
